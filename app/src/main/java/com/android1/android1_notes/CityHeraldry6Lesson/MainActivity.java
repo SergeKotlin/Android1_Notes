903ad60f13ec93eef112526f0505c858d1b59346
Serge <jobsfolder@yandex.ru>
2021-04-28 20:42:22 +0300

    add CityHeraldry example - Lesson 2 (from Android1)

---
+++ app/src/main/java/com/android1/android1_notes/CityHeraldry6Lesson/MainActivity.java
@@ -0,0 +1,14 @@
+package com.android1.android1_notes.CityHeraldry6Lesson;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
