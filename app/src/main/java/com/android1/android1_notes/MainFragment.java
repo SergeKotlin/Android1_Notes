a1d9ad943aaf98c3aafb226aeb910326a82dab87
SeregaWorkNetbook <jobsfolder@yandex.ru>
2021-05-05 18:45:25 +0300

    init Lesson 7 (from Android1)

---
+++ app/src/main/java/com/android1/android1_notes/MainFragment.java
@@ -0,0 +1,20 @@
+package com.android1.android1_notes;
+
+import android.content.pm.LabeledIntent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class MainFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView (LayoutInflater inflater, @Nullable ViewGroup container,
+                              @Nullable Bundle saveInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+}
