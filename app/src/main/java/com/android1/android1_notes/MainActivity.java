2e9fe755410d13e240aa40a33af0bbb3af47f6f2
Serge <jobsfolder@yandex.ru>
2021-06-15 03:34:51 +0300

    init

--- app/src/main/java/com/android1/android1_notes/MainActivity.java
+++ app/src/main/java/com/android1/android1_notes/MainActivity.java
@@ -1,14 +1,195 @@
 package com.android1.android1_notes;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.content_main);
+        readSettings();
+        initView();
+    }
+
+    // Чтение настроек
+    private void readSettings(){
+        // Специальный класс для хранения настроек
+        SharedPreferences sharedPref = getSharedPreferences(Settings.SHARED_PREFERENCE_NAME, MODE_PRIVATE);
+        // Считываем значения настроек
+        Settings.isBackStack = sharedPref.getBoolean(Settings.IS_BACK_STACK_USED, false);
+        Settings.isAddFragment = sharedPref.getBoolean(Settings.IS_ADD_FRAGMENT_USED, true);
+        Settings.isBackAsRemove = sharedPref.getBoolean(Settings.IS_BACK_AS_REMOVE_FRAGMENT, true);
+        Settings.isDeleteBeforeAdd = sharedPref.getBoolean(Settings.IS_DELETE_FRAGMENT_BEFORE_ADD, false);
+    }
+
+    private void initView() {
+        initToolbar();
+        initButtonMain();
+        initButtonFavorite();
+        initButtonSettings();
+        initButtonBack();
+    }
+
+    private void initToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Обработка выбора пункта меню приложения (активити)
+        int id = item.getItemId();
+
+        switch(id){
+            case R.id.action_settings:
+                addFragment(new SettingsFragment());
+                return true;
+            case R.id.action_main:
+                addFragment(new MainFragment());
+                return true;
+            case R.id.action_favorite:
+                addFragment(new FavoriteFragment());
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Здесь определяем меню приложения (активити)
+        getMenuInflater().inflate(R.menu.main, menu);
+        MenuItem search = menu.findItem(R.id.action_search); // поиск пункта меню поиска
+        SearchView searchText = (SearchView) search.getActionView(); // строка поиска
+        searchText.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            // реагирует на конец ввода поиска
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Toast.makeText(MainActivity.this, query, Toast.LENGTH_SHORT).show();
+                return true;
+            }
+            // реагирует на нажатие каждой клавиши
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return true;
+            }
+        });
+
+        return true;
+    }
+
+    private void initButtonMain() {
+        Button buttonMain = findViewById(R.id.buttonMain);
+        buttonMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addFragment(new MainFragment());
+            }
+        });
+    }
+
+    private void initButtonFavorite() {
+        Button buttonFavorite = findViewById(R.id.buttonFavorite);
+        buttonFavorite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addFragment(new FavoriteFragment());
+            }
+        });
+    }
+
+    private void initButtonSettings() {
+        Button buttonSettings = findViewById(R.id.buttonSettings);
+        buttonSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addFragment(new SettingsFragment());
+            }
+        });
+    }
+
+    private void addFragment(Fragment fragment){
+
+        //Получить менеджер фрагментов
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        // Открыть транзакцию
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        // Удалить видимый фрагмент
+        if (Settings.isDeleteBeforeAdd){
+            Fragment fragmentToRemove = getVisibleFragment(fragmentManager);
+            if (fragmentToRemove != null) {
+                fragmentTransaction.remove(fragmentToRemove);
+            }
+        }
+
+        // Добавить фрагмент
+        if (Settings.isAddFragment) {
+            fragmentTransaction.add(R.id.fragment_container, fragment);
+        } else {
+            fragmentTransaction.replace(R.id.fragment_container, fragment);
+        }
+
+        // Добавить транзакцию в бэкстек
+        if (Settings.isBackStack){
+            // Пояснение: внутри созданной fragmentTransaction происходят действия .add(), .replace(),
+            // которые можно отправить в стек обратного вызова с помощью .addToBackStack(null).
+            // Если указать имя, можно будет перепрыгивать сразу к именованной транзакции с
+            // определённым фрагментом fragmentManager.popBackStack(name, flags). Где flags - признак,
+            // будем ли включать именованную транзакцию. 0 - фрагмент с именованной транзакцией, 1 или
+            // POP_BACK_STACK_INCLUSIVE - предыдущее состояние относительно именованной транзакции.
+            fragmentTransaction.addToBackStack(null);
+        }
+
+        // Закрыть транзакцию
+        fragmentTransaction.commit();
+    }
+
+    private void initButtonBack() {
+        Button buttonBack = findViewById(R.id.buttonBack);
+        buttonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                if (Settings.isBackAsRemove){
+                    Fragment fragment = getVisibleFragment(fragmentManager);
+                    if (fragment != null) {
+                        fragmentManager.beginTransaction().remove(fragment).commit();
+                    }
+                } else {
+                    fragmentManager.popBackStack();
+                }
+            }
+        });
+    }
+
+    private Fragment getVisibleFragment(FragmentManager fragmentManager){
+        List<Fragment> fragments = fragmentManager.getFragments();
+        int countFragments = fragments.size();
+        for(int i = countFragments - 1; i >= 0; i--){
+            Fragment fragment = fragments.get(i);
+            if(fragment.isVisible())
+                return fragment;
+        }
+        return null;
     }
 }
+
