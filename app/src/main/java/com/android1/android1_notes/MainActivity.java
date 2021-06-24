4c90292dbf104a196ee0bbd4f42b11b7ed2a85b9
Serge <jobsfolder@yandex.ru>
2021-06-24 08:32:30 +0300

    important fix  MainActivity

--- app/src/main/java/com/android1/android1_notes/MainActivity.java
+++ app/src/main/java/com/android1/android1_notes/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.content_main);
+        setContentView(R.layout.activity_main);
         readSettings();
         initView();
     }
