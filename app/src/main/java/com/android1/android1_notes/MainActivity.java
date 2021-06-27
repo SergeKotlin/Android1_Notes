48eddf0614df0180c86178eea5246b4c39169891
Serge <jobsfolder@yandex.ru>
2021-06-27 23:12:02 +0300

    add last point - side bar/menu (Up+ Lesson 7_2)

--- app/src/main/java/com/android1/android1_notes/MainActivity.java
+++ app/src/main/java/com/android1/android1_notes/MainActivity.java
@@ -9,12 +9,17 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.List;
 
@@ -40,37 +45,67 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initView() {
-        initToolbar();
+//        Toolbar toolbar = initToolbar();
+        initDrawer(initToolbar());
         initButtonMain();
         initButtonFavorite();
         initButtonSettings();
         initButtonBack();
     }
 
-    private void initToolbar() {
+    private Toolbar initToolbar() {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        return toolbar;
     }
 
-    @SuppressLint("NonConstantResourceId")
+    // регистрация drawer
+    private void initDrawer(Toolbar toolbar) {
+        final DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        // Обработка навигационного меню
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(item -> {
+            int id = item.getItemId();
+            if (navigateFragment(id)){
+                drawer.closeDrawer(GravityCompat.START);
+                return true;
+            }
+            return false;
+        });
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Обработка выбора пункта меню приложения (активити)
         int id = item.getItemId();
-
-        switch(id){
-            case R.id.action_settings:
-                addFragment(new SettingsFragment());
-                return true;
-            case R.id.action_main:
-                addFragment(new MainFragment());
-                return true;
-            case R.id.action_favorite:
-                addFragment(new FavoriteFragment());
-                return true;
+        if (navigateFragment(id)) {
+            return true;
         }
         return super.onOptionsItemSelected(item);
     }
+
+    @SuppressLint("NonConstantResourceId")
+    private boolean navigateFragment(int id) {
+            switch(id){
+                case R.id.action_settings:
+                    addFragment(new SettingsFragment());
+                    return true;
+                case R.id.action_main:
+                    addFragment(new MainFragment());
+                    return true;
+                case R.id.action_favorite:
+                    addFragment(new FavoriteFragment());
+                    return true;
+            }
+            return false;
+        }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
